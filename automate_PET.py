5a5d7203649a853e78b5a557d4e2f661282974b2
Rebekah Wickens <rwickens@gmail.com>
2019-10-31 17:46:57 -0400

    Automate Bédard lab's PET/MINC processing pipeline

--- automate_PET.py
+++ automate_PET.py
@@ -5,7 +5,8 @@
 # Example input to run program: /home/minc/projectfile/automate_PET.py 102 8.4 /home/minc/projectfolder/patientfolder
 # Assumes you are in a project directory containing patients' folders.
 # In this patient folder, you must have the IT file, TAL file, GRID file, and T1 file from CIVET,
-# In the project directory, keep the json configuration file. Here, you can adjust defaults (e.g., mask used, standard template used)
+# In the project directory, keep the json configuration file. In this file, you can change defaults (e.g, mask used, standard template used) 
+# If the json config is not present, program will look for WM mask file (WM_0.99_new.mnc) nd MNI standard template file (mni_icbm152_t1_tal_nlin_sym_09c.mnc) in this project folder.
 # Note: This program will overwrite files (if the file name of one of your ouputs already exists).   
 """
 
