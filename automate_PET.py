1271b1183860dd4362a0556138c79139eddbf858
Rebekah Wickens <rwickens@gmail.com>
2019-11-03 20:05:49 -0500

    Update automate_PET.py

--- automate_PET.py
+++ automate_PET.py
@@ -3,7 +3,7 @@
 """-----------INFORMATION FOR USER:----------- 
 # Three inputs to run the program: weight (kg), dose (mCi), and patient folder (full path needed)
 # Example input to run program: python /home/minc/projectfolder/automate_PET.py 102 8.4 /home/minc/projectfolder/patientfolder
-# Assumes you are in a project directory containing patients' folders.
+# Assumes you have a project directory containing patients' folders.
 # In this patient folder, you must have the IT file, TAL file, GRID file, and T1 file from CIVET,
 # In the project directory, keep the json configuration file. In this file, you can change defaults (e.g, mask (reference tissue) used, standard template used) 
 # If the json config is not present, program will look for WM mask file (WM_0.99_new.mnc) and MNI standard template file (mni_icbm152_t1_tal_nlin_sym_09c.mnc) in this project folder.
